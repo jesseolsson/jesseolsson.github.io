e8d6d05792467057d10fecdb3ac05c30ba747f03
jesseolsson <jesseolsson@Jesses-MacBook-Air.local>
2014-08-04 21:57:35 -0400

    added babylon.js for the first time

--- _site/index.js
+++ _site/index.js
@@ -2,7 +2,26 @@
   var main;
 
   main = function() {
-    return document.body.innerHTML = 'Hello, World!';
+    var canvas, engine, onSceneLoad;
+    canvas = document.querySelector('canvas');
+    engine = new BABYLON.Engine(canvas, true);
+    onSceneLoad = function(scene) {
+      var onSceneReady;
+      onSceneReady = function() {
+        var onResize, render;
+        onResize = function() {
+          return engine.resize();
+        };
+        window.onresize = onResize;
+        scene.activeCamera.attachControl(canvas);
+        render = function() {
+          return scene.render();
+        };
+        return engine.runRenderLoop(render);
+      };
+      return scene.executeWhenReady(onSceneReady);
+    };
+    return BABYLON.SceneLoader.Load('', 'index.babylon', engine, onSceneLoad);
   };
 
   window.onload = main;
--- index.coffee
+++ index.coffee
@@ -2,6 +2,30 @@
 ---
 
 main = ->
-  document.body.innerHTML = 'Hello, World!'
+  canvas = document.querySelector('canvas')
+  engine = new BABYLON.Engine(canvas, true)
+
+  onSceneLoad = (scene) ->
+
+    onSceneReady = ->
+
+      onResize = ->
+        engine.resize()
+
+      window.onresize = onResize
+      scene.activeCamera.attachControl(canvas)
+
+      render = ->
+        scene.render()
+
+      engine.runRenderLoop(render)
+
+    scene.executeWhenReady(onSceneReady)
+
+  BABYLON.SceneLoader.Load(
+    '',
+    'index.babylon',
+    engine,
+    onSceneLoad)
 
 window.onload = main
