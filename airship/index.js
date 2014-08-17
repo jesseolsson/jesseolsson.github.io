8f009da6c0bf98e9333eaec4287d6fe2a129e908
jesseolsson <jesseolsson@Jesses-MacBook-Air.local>
2014-08-16 20:15:29 -0400

    added sound

--- airship/index.js
+++ airship/index.js
@@ -1,7 +1,4 @@
-var keydown = false
-
 window.on('load', function() {
-  setupController()
   var engine = new BABYLON.Engine($('canvas'), true)
   enableEngineResizing(engine)
   var scene = setupScene(engine)
@@ -16,25 +13,10 @@ function enableEngineResizing(engine) {
 function setupScene(engine){
   BABYLON.SceneLoader.Load('', 'airship.babylon', engine, function(scene) {
     scene.executeWhenReady(function() {
-
-      scene.enablePhysics()
-      var sphere = scene.getMeshByName('Sphere')
-      sphere.setPhysicsState({impostor: BABYLON.PhysicsEngine.SphereImpostor, mass: 1, restitution: .1})
-      var plane = scene.getMeshByName('Plane')
-      plane.setPhysicsState({impostor: BABYLON.PhysicsEngine.PlaneImpostor, mass: 0})
-      scene.activeCamera.attachControl($('canvas'))
-
+      codeScene(scene)
       setupLoop(engine, scene)
     })
   })
-}
-
-function beforeRender(scene) {
-  if (keydown) {
-    var player = scene.getMeshByName('player')
-    var forwards = new BABYLON.Vector3(-.01, 0, 0)
-    player.moveWithCollisions(forwards)
-  }
 }
 
 function setupLoop(engine, scene) {
@@ -46,11 +28,12 @@ function setupLoop(engine, scene) {
   })
 }
 
-function setupController() {
-  window.on('keydown', function() {
-    keydown = true
-  })
-  window.on('keyup', function() {
-    keydown = false
-  })
+function codeScene(scene) {
+  scene.activeCamera.attachControl($('canvas'))
+  scene.activeCamera.maxZ = 2000
+
+}
+
+function beforeRender(scene) {
+
 }
