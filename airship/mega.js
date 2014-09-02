1d1f0f9f3cb60774a478815936b50be2bf6161cf
jesseolsson <jesseolsson@Jesses-MacBook-Air.local>
2014-09-01 23:21:28 -0400

    added html5

--- airship/mega.js
+++
@@ -1,7 +0,0 @@
-function $(selector) {
-  return document.querySelector(selector)
-}
-window.on = function(eventType, handler) {
-  this.addEventListener(eventType, handler)
-}
-Element.prototype.on = window.on
